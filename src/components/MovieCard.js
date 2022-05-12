19b0f0b692ff8d9394cd57d4f67c238abccee46b
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-05-12 15:53:37 -0600

    Hide poster image on hover

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./MovieCard.css";
 
-function MovieCard({key, id, poster, backdrop, title, rating, date}) {
+function MovieCard({id, poster, backdrop, title, rating, date}) {
   return (
     <div className="movie-card">
       <div className="rating-box">
         <p>{rating.toFixed(1)}</p>
       </div>
       <img className="poster-image" src={poster}/>
-
+      <div className="movie-card-hover">
+        <img className="backdrop-image" src={backdrop} />
+        <p className="card-date">{date}</p>
+        <p className="card-title">{title}</p>
+      </div>
     </div>
   )
 }
