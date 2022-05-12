62506f14f67fe4b7680046faa6fec0ed18fabb30
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-05-12 13:38:51 -0600

    Add movieData with componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
+import movieData from "./movieData";
 import "./App.css";
-
+console.log(movieData.movies)
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      movies: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      movies: movieData.movies
+    })
   }
 
   render() {
     return (
-      <h1 className="App">Test</h1>
+        <h1 className="App">Test</h1>
+
     )
   }
 }
