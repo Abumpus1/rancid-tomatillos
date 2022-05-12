8d842f050d78f4842043bf31b7011b5524965f0a
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-05-12 15:16:46 -0600

    Set up CSS for poster image

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,6 +4,9 @@ import "./MovieCard.css";
 function MovieCard({key, id, poster, backdrop, title, rating, date}) {
   return (
     <div className="movie-card">
+      <div className="rating-box">
+        <p>{rating.toFixed(1)}</p>
+      </div>
       <img className="poster-image" src={poster}/>
 
     </div>
