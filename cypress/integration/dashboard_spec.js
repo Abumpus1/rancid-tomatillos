173fa5ca469a389fb56e420b8bf397510ddb799c
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-05-16 18:53:41 -0600

    Add top section tests

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -37,7 +37,7 @@ describe("Dashboard spec testing", () => {
       
   });
 
-  it("should display an error message on a 500 server error", () => {
+  it("Should display an error message on a 500 server error", () => {
     cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies/", {
       statusCode: 500,
       body: {
@@ -48,4 +48,23 @@ describe("Dashboard spec testing", () => {
     cy.get("h2").contains("An error has occured, please try your request again later.")
   });
 
+  it("Should have a title and logo", () => {
+    cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies/", {
+      statusCode: 201,
+      body: movieData
+    })
+    
+    cy.get("h1").should("have.text", "Rancid Tomatillos")
+
+    cy.get(".top-section img").should("have.attr", "src").should("include", "/static/media/logo.6ce24c58023cc2f8fd88fe9d219db6c6.svg")
+  });
+
+  it("Should not have a home button on dashboard", () => {
+    cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies/", {
+      statusCode: 201,
+      body: movieData
+    })
+
+    cy.get(".top-section button").should("not.exist")
+  });
 });
